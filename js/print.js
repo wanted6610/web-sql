b18db3b7aee61d1439ff1cfd887229e5248dd600
wanted6610 <56591559+wanted6610@users.noreply.github.com>
2021-07-15 00:01:56 +0300

    Delete print.js

--- js/print.js
+++
@@ -1,8 +0,0 @@
-function PrintData()
-{
-   let divToPrint = document.getElementById("base-table");
-   newWin= window.open("");
-   newWin.document.write(divToPrint.outerHTML);
-   newWin.print();
-   newWin.close();
-}
